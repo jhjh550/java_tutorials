38bf2bd7d4ef17e57840b28e749105d6c8a02054
Mirim <Mirim@mirim.com>
2015-08-27 20:15:05 +0900

    first commit

--- src/com/company/t05_sychronized/Calculate.java
+++ src/com/company/t05_sychronized/Calculate.java
@@ -1,5 +1,7 @@
 package com.company.t05_sychronized;
 
+import java.util.Objects;
+
 /**
  * Created by Mirim on 2015-08-25.
  */
@@ -9,9 +11,10 @@ public class Calculate {
         amount = 0;
     }
 
+    private Object lock = new Object();
     //public synchronized void plus(int value){
     public void plus(int value){
-        synchronized (this) {
+        synchronized (lock) {
             amount += value;
         }
     }
