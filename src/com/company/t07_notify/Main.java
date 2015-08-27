1ab963403a1a71cd03543b110ba2a8b3a4aeacbe
Mirim <Mirim@mirim.com>
2015-08-27 21:17:17 +0900

    first commit

--- src/com/company/t07_notify/Main.java
+++ src/com/company/t07_notify/Main.java
@@ -3,6 +3,38 @@ package com.company.t07_notify;
 public class Main {
 
     public static void main(String[] args) {
+        Object monitor = new Object();
+        StateThread thread = new StateThread(monitor);
 
+        System.out.println("thread state="+thread.getState());
+        thread.start();
+        System.out.println(
+                "thread state(after start)=" + thread.getState());
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(
+                "thread state(0.1sec)="+thread.getState());
+
+        synchronized (monitor){
+            monitor.notify();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
