fac0eb02761bc5e4ace48e1f2c3a28707d30a984
Mirim <Mirim@mirim.com>
2015-08-27 20:42:01 +0900

    first commit

---
+++ src/com/company/t06_intterrupt/Main.java
@@ -0,0 +1,8 @@
+package com.company.t06_intterrupt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/company/t06_intterrupt/SleepThread.java
@@ -0,0 +1,31 @@
+package com.company.t06_intterrupt;
+
+/**
+ * Created by Mirim on 2015-08-27.
+ */
+public class SleepThread extends Thread {
+    long sleepTime;
+
+    public SleepThread(long sleepTime){
+        this.sleepTime = sleepTime;
+    }
+
+    public void run(){
+        System.out.println("Sleeping "+getName());
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Stopping " + getName());
+    }
+}
+
+
+
+
+
+
+
+
+
