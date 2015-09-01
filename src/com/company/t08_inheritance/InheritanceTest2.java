1747d530da0943aee335a36492427487ee5c460e
Mirim <Mirim@mirim.com>
2015-09-01 19:16:39 +0900

    first commit

--- src/com/company/t08_inheritance/InheritanceTest2.java
+++ src/com/company/t08_inheritance/InheritanceTest2.java
@@ -5,6 +5,25 @@ package com.company.t08_inheritance;
  */
 public class InheritanceTest2 {
     public static void main(String[] args){
+        class Parent2{
+            int i =7;
+            public int get(){
+                return i;
+            }
+        }
 
+        class Child2{
+            int i=5;
+            public int get(){
+                return i;
+            }
+        }
+
+
+
+        Parent2 p = new Parent2();
+        System.out.println("------------1------------");
+        System.out.println(p.i);
+        System.out.println(p.get());
     }
 }
