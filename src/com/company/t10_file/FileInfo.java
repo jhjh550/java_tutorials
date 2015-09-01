14c043acc61dbea00e04fdd316c5a70aeea27cef
Mirim <Mirim@mirim.com>
2015-09-01 20:17:08 +0900

    first commit

---
+++ src/com/company/t10_file/FileInfo.java
@@ -0,0 +1,13 @@
+package com.company.t10_file;
+
+import java.io.File;
+
+/**
+ * Created by Mirim on 2015-09-01.
+ */
+public class FileInfo {
+    public static void main(String[] args){
+        String filePath = "C:"+File.separator+"Temp"+File.separator+"SLog.log";
+
+    }
+}
