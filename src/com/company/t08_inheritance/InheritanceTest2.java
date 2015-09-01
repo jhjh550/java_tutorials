7852a205c66a48919ea9110afcbf1ab94227b1c8
Mirim <Mirim@mirim.com>
2015-09-01 19:19:58 +0900

    first commit

--- src/com/company/t08_inheritance/InheritanceTest2.java
+++ src/com/company/t08_inheritance/InheritanceTest2.java
@@ -12,7 +12,7 @@ public class InheritanceTest2 {
             }
         }
 
-        class Child2{
+        class Child2 extends Parent2{
             int i=5;
             public int get(){
                 return i;
@@ -25,5 +25,15 @@ public class InheritanceTest2 {
         System.out.println("------------1------------");
         System.out.println(p.i);
         System.out.println(p.get());
+
+        Child2 c = new Child2();
+        System.out.println("------------2------------");
+        System.out.println(c.i);
+        System.out.println(c.get());
+
+        Parent2 p2 = new Child2();
+        System.out.println("------------3------------");
+        System.out.println(p2.i);
+        System.out.println(p2.get());
     }
 }
