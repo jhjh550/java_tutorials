295d804a517439d5764abf1ab482a7efc129da31
Mirim <Mirim@mirim.com>
2015-09-01 19:59:15 +0900

    first commit

---
+++ src/com/company/t08_inheritance/t09_bufferedreader.java
@@ -0,0 +1,14 @@
+package com.company.t08_inheritance;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Mirim on 2015-09-01.
+ */
+public class t09_bufferedreader {
+    public static void main(String[] args){
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+    }
+}
