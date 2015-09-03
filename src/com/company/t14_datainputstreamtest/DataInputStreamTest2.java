c52b34be49f10b0ea44e1078c84d6382a67933b6
Mirim <Mirim@mirim.com>
2015-09-03 19:43:24 +0900

    first commit

---
+++ src/com/company/t14_datainputstreamtest/DataInputStreamTest2.java
@@ -0,0 +1,10 @@
+package com.company.t14_datainputstreamtest;
+
+/**
+ * Created by Mirim on 2015-09-03.
+ */
+public class DataInputStreamTest2 {
+    public static void main(String[] args){
+
+    }
+}
