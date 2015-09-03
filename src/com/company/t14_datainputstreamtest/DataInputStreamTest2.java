502fe42311add2a87b1c00a2a5d3deb8e82145c2
Mirim <Mirim@mirim.com>
2015-09-03 19:58:41 +0900

    first commit

--- src/com/company/t14_datainputstreamtest/DataInputStreamTest2.java
+++ src/com/company/t14_datainputstreamtest/DataInputStreamTest2.java
@@ -15,6 +15,17 @@ public class DataInputStreamTest2 {
             fis = new FileInputStream("C:\\Temp\\myTest.bin");
             dis = new DataInputStream(fis);
 
+            boolean b   = dis.readBoolean();
+            byte b2     = dis.readByte();
+            int i       = dis.readInt();
+            double d    = dis.readDouble();
+            String s    = dis.readUTF();
+
+            System.out.println("boolean : "+b);
+            System.out.println("byte: " + b2);
+            System.out.println("int : "+i);
+            System.out.println("double : "+d);
+            System.out.println("string : "+s);
 
         }catch (Exception e){
             e.printStackTrace();
