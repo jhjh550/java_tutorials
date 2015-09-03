3b054a5551554b3fd7bc527cdcf4801e61d3d1f0
Mirim <Mirim@mirim.com>
2015-09-03 19:54:20 +0900

    first commit

--- src/com/company/t14_datainputstreamtest/DataInputStreamTest2.java
+++ src/com/company/t14_datainputstreamtest/DataInputStreamTest2.java
@@ -1,10 +1,23 @@
 package com.company.t14_datainputstreamtest;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
 /**
  * Created by Mirim on 2015-09-03.
  */
 public class DataInputStreamTest2 {
     public static void main(String[] args){
+        FileInputStream fis = null;
+        DataInputStream dis = null;
 
+        try {
+            fis = new FileInputStream("C:\\Temp\\myTest.bin");
+            dis = new DataInputStream(fis);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
