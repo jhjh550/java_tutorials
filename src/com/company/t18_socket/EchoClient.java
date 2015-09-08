f55ecd3ec0943ebabc203f6144519c75bde280a7
Mirim <Mirim@mirim.com>
2015-09-08 20:59:01 +0900

    first commit

--- src/com/company/t18_socket/EchoClient.java
+++ src/com/company/t18_socket/EchoClient.java
@@ -1,5 +1,6 @@
 package com.company.t18_socket;
 
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -9,8 +10,25 @@ public class EchoClient {
     public static void main(String[] args){
         try {
             Socket socket = new Socket("127.0.0.1", 10000);
+            BufferedReader keyboard =
+                    new BufferedReader(new InputStreamReader(System.in));
+
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+            PrintWriter pw
+                    = new PrintWriter(new OutputStreamWriter(out));
+            BufferedReader br
+                    = new BufferedReader(new InputStreamReader(in));
+            String line = null;
+
+
+
+
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 }
+
+
+
