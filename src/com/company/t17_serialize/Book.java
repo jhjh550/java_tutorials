f467e4a72ad91471aab681dea9324c657e870f1e
Mirim <Mirim@mirim.com>
2015-09-08 18:56:40 +0900

    first commit

--- src/com/company/t17_serialize/Book.java
+++ src/com/company/t17_serialize/Book.java
@@ -5,4 +5,46 @@ package com.company.t17_serialize;
  */
 public class Book {
     private String isbn;
+    private String title;
+    private String author;
+    private int price;
+
+    public Book(String isbn, String title, String author, int price) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
