9d1124ff3ca850aaaab49d83645f8e92e8207c45
Mirim <Mirim@mirim.com>
2015-09-08 18:53:18 +0900

    first commit

--- src/com/company/t16_filecopy/FileCopy.java
+++ src/com/company/t16_filecopy/FileCopy.java
@@ -1,10 +1,29 @@
 package com.company.t16_filecopy;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+
 /**
  * Created by Mirim on 2015-09-03.
  */
 public class FileCopy {
     public static void main(String[] args){
+
+        FileReader fr = null;
+        FileWriter fw = null;
+
+        try {
+            fr = new FileReader("C:\\Temp\\pg1661.txt");
+            fw = new FileWriter("C:\\Temp\\pg1661_copy.txt");
+            char[] buffer = new char[512];
+            int readcount= 0;
+            while((readcount = fr.read(buffer)) != -1){
+                fw.write(buffer,0,readcount);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ src/com/company/t17_serialize/Book.java
@@ -0,0 +1,8 @@
+package com.company.t17_serialize;
+
+/**
+ * Created by Mirim on 2015-09-08.
+ */
+public class Book {
+    private String isbn;
+}
