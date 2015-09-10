717332ff697fa55c2f57e5195b0812d1a4f96a01
Mirim <Mirim@mirim.com>
2015-09-10 19:03:54 +0900

    first commit

--- src/com/company/t19_multithreadsocket/EchoThreadServer.java
+++ src/com/company/t19_multithreadsocket/EchoThreadServer.java
@@ -1,7 +1,28 @@
 package com.company.t19_multithreadsocket;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Mirim on 2015-09-10.
  */
 public class EchoThreadServer {
+    public static void main(String[] args){
+        try{
+            ServerSocket server = new ServerSocket(10001);
+            System.out.println("접속대기....");
+
+            while (true){
+                Socket socket = server.accept();
+                EchoThread th = new EchoThread(socket);
+                th.start();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        class EchoThread extends Thread{
+
+        }
+    }
 }
