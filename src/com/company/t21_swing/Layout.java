d3bff163fe5e3ce85b109de3e82683cc8e84ded4
Mirim <Mirim@mirim.com>
2015-09-15 21:13:08 +0900

    first commit

---
+++ src/com/company/t21_swing/Layout.java
@@ -0,0 +1,12 @@
+package com.company.t21_swing;
+
+import javax.swing.*;
+
+/**
+ * Created by Mirim on 2015-09-15.
+ */
+public class Layout extends JFrame {
+    public static void main(String[] args){
+
+    }
+}
