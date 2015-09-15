33afe199fc6f049936594fa47f7ba87e2a684a84
Mirim <Mirim@mirim.com>
2015-09-15 20:58:18 +0900

    first commit

--- src/com/company/t21_swing/FirstWindow.java
+++ src/com/company/t21_swing/FirstWindow.java
@@ -5,11 +5,20 @@ import javax.swing.*;
 /**
  * Created by Mirim on 2015-09-15.
  */
-public class FirstWindow {
+public class FirstWindow extends JFrame{
+    public FirstWindow(String title){
+        setSize(300,200);
+        setTitle(title);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args){
-        JFrame fw = new JFrame();
-        fw.setTitle("FirstWindow");
-        fw.setSize(300,200);
-        fw.setVisible(true);
+        FirstWindow fw = new FirstWindow("First Window");
+
+//        JFrame fw = new JFrame();
+//        fw.setTitle("FirstWindow");
+//        fw.setSize(300,200);
+//        fw.setVisible(true);
     }
 }
