0d2f589a074958aee774fba5fe5152582837ee36
Mirim <Mirim@mirim.com>
2015-09-15 20:43:07 +0900

    first commit

--- src/com/company/t21_swing/FirstWindow.java
+++ src/com/company/t21_swing/FirstWindow.java
@@ -1,7 +1,15 @@
 package com.company.t21_swing;
 
+import javax.swing.*;
+
 /**
  * Created by Mirim on 2015-09-15.
  */
 public class FirstWindow {
+    public static void main(String[] args){
+        JFrame fw = new JFrame();
+        fw.setTitle("FirstWindow");
+        fw.setSize(300,200);
+        fw.setVisible(true);
+    }
 }
