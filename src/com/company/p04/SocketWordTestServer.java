f08c3d014c617723b011e2a06f092913218be88d
Mirim <Mirim@mirim.com>
2015-09-22 19:39:16 +0900

    first commit

---
+++ src/com/company/p04/SocketWordTestServer.java
@@ -0,0 +1,50 @@
+package com.company.p04;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by Mirim on 2015-09-22.
+ */
+public class SocketWordTestServer {
+    public static void main(String[] args) {
+        try {
+            System.out.println("접속대기...");
+            ServerSocket server = new ServerSocket(10000);
+
+            Socket socket = server.accept();
+
+
+            InetAddress address = socket.getInetAddress();
+            System.out.println(address.getHostAddress() + " 로부터 접속했습니다.");
+
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+
+
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(in));
+            PrintWriter pw =
+                    new PrintWriter(new OutputStreamWriter(out));
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println("client : " + line);
+
+//                String str = keyboard.readLine();
+//                pw.println(str);
+//                pw.flush();
+            }
+
+            pw.close();
+            br.close();
+            socket.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
