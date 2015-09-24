58a4a0b2cc3960fa3bdc51a112c5ae036eb116b8
Mirim <Mirim@mirim.com>
2015-09-24 19:40:36 +0900

    first commit

--- src/com/company/t22_urltest/urlconnectiontest.java
+++ src/com/company/t22_urltest/urlconnectiontest.java
@@ -3,6 +3,7 @@ package com.company.t22_urltest;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -14,7 +15,9 @@ public class urlconnectiontest {
     public static void main(String[] args){
         URL url = null;
         try {
-            url = new URL("http://www.daum.net");
+            url = new URL("http://search.daum.net/"+
+                    "search?w=tot&DA=YZR&t__nil_searchbox=btn"
+                    +"&sug=&sugo=&sq=&o=&q=apple");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -25,7 +28,15 @@ public class urlconnectiontest {
             String contentType = urlConn.getContentType();
             System.out.println(contentType);
 
-            InputStream in = urlConn.getInputStream();
+            HttpURLConnection httpUrlConn = (HttpURLConnection)urlConn;
+            httpUrlConn.setRequestMethod("GET");
+            httpUrlConn.setDoOutput(true);
+            httpUrlConn.setDoInput(true);
+            httpUrlConn.setUseCaches(false);
+            httpUrlConn.setDefaultUseCaches(false);
+
+
+
 
 
         }catch (Exception e){
